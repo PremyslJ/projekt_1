71b13c261e7595c075ed23c97859aaad6fd81eaf
PremyslJ <j.prema@seznam.cz>
2025-01-11 12:54:22 +0100

    1. oprava překlepu "upper_case" 2.Odstranění přebytečných znaku (ká to kopíroval z originálního textu a nějak jsem si toho nevšiml) 3.zrušen "words" 4. zrušen "bar_width" 5. Cyklus for, vloženo pod jeden cyklus. (Snad jsem to pochopil správně) 5. opraven vstup na nekonecně mnoho textů.

--- Projekt_1.py
+++ Projekt_1.py
@@ -31,7 +31,17 @@ are found in multiple limestone layers, which lie some
 represent several varieties of perch, as well as
 other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
+
 ]
 # Seznam registrovanych uzivatelu
 REGISTERED_USERS = {
@@ -53,14 +63,14 @@ if REGISTERED_USERS.get(user_name) == password:
 
    
     # Vyber textu k analyze
-    selection_of_text = input("Enter a number btw. 1 and 3 to select: ")
+    selection_of_text = input("Enter a number btw. 1 and {} to select: ".format(len(TEXTS)))
     print("-"*40)
 
     # Kontrola spravneho vstupu
     if not selection_of_text.isdigit():
         print("Wrong input. enter number!!!")
         exit()
-    elif not (1 <= int(selection_of_text) <= 3):
+    elif not (1 <= int(selection_of_text) <= len(TEXTS)):
         print("the number is not in the required range!!!")
         exit()
 
@@ -69,10 +79,8 @@ if REGISTERED_USERS.get(user_name) == password:
     vyber_textu = TEXTS[int(selection_of_text)-1]
         
     # Rozdeleni textu na slova
-    words =[] 
     words =vyber_textu.split()
     words_count = len(words)
-
 
     # Ocisteni slov od tecek a carek
     cleaned_words = []                                         
@@ -82,34 +90,26 @@ if REGISTERED_USERS.get(user_name) == password:
 
     # Pocet slov podle ruznych kriterii
     titlecase_words = 0
+    uppercace_words = 0
+    lowercase_words= 0
+    numeric_strings = 0
+    sum_numbers = 0
     for word in cleaned_words:
         if word.istitle():
             titlecase_words += 1
-
-    uppersace_words = 0
-    for word in cleaned_words:
         if word.isupper() and word.isalpha():
-            uppersace_words += 1
-                
-    lowercase_words= 0
-    for word in cleaned_words:
+            uppercace_words += 1
         if word.islower():
             lowercase_words += 1
-
-    numeric_strings = 0
-    for word in cleaned_words:
         if word.isdigit():
             numeric_strings+= 1
-
-    sum_numbers = 0
-    for word in cleaned_words:
         if word.isdigit():
             sum_numbers+=int(word)
 
     # Vypis vysledku
     print("There are", words_count, "words in the selected text.")
     print("There are",titlecase_words,"titlecase words.")
-    print("There are",uppersace_words, "uppercase words.")
+    print("There are",uppercace_words, "uppercase words.")
     print("There are",lowercase_words,"lowercase words.")
     print("There are",numeric_strings,"numeric strings.")
     print("The sum of all the numbers ",sum_numbers)
@@ -122,20 +122,20 @@ if REGISTERED_USERS.get(user_name) == password:
 
     max_len = max(number_of_words)
     max_count = max(number_of_words.count(i) for i in number_of_words) 
-    bar_width = max_count
+   # bar_width = max_count
 
     
     print("-" * 40)
-    print(f"{'LEN':>3} | {'OCCURRENCES'.ljust(bar_width)} | {'NR.':>3}")
+    print(f"{'LEN':>3} | {'OCCURRENCES'.ljust(max_count)} | {'NR.':>3}")
     print("-" * 40)
 
     for i in range(1, max_len + 1):
         count = number_of_words.count(i)  
         if count > 0:  
-            print(f"{i:>3} | {'*' * count:<{bar_width}} | {count:>3}")
+            print(f"{i:>3} | {'*' * count:<{max_count}} | {count:>3}")
     
 else:   
-    print("\n","unregistered user, terminating the program.¨¨")
+    print("\n","unregistered user, terminating the program.")
     exit()
 
     
