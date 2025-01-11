2aac4803693439b677ab17e527aad989d760434f
PremyslJ <j.prema@seznam.cz>
2025-01-11 12:58:44 +0100

    Výmaz čtvrtého textu

--- Projekt_1.py
+++ Projekt_1.py
@@ -31,17 +31,7 @@ are found in multiple limestone layers, which lie some
 represent several varieties of perch, as well as
 other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.''',
-'''The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
-
 ]
 # Seznam registrovanych uzivatelu
 REGISTERED_USERS = {
